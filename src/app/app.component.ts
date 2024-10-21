783c3d0331c2554adc7ff004b319f3743d0d55da
eugenekx <kx.eugene@gmail.com>
2024-10-21 11:28:17 +0300

    add datatable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  category: string;
+  date: string;
+  status: 'available' | 'out of stock' | 'pending';
+  image: string;
+  rating: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [AgGridAngular], // Add Angular Data Grid Component
+  styleUrl: './app.component.scss',
+  template: `
+    <!-- The AG Grid component -->
+    <ag-grid-angular
+      class="ag-theme-material-dark"
+      style="height: 100%;"
+      [rowData]="rowData"
+      [columnDefs]="colDefs"
+    />
+  `,
 })
 export class AppComponent {
-  title = 'angular-marketplace';
+  product$: Observable<Product[]>;
+  firestore: Firestore = inject(Firestore);
+  rowData: Product[] = [];
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: 'id' },
+    { field: 'name' },
+    { field: 'description' },
+    { field: 'price' },
+    { field: 'stock' },
+    { field: 'category' },
+    { field: 'date' },
+    { field: 'status' },
+    { field: 'image' },
+    { field: 'rating' },
+  ];
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'products');
+    this.product$ = collectionData<Product>(itemCollection);
+  }
+
+  ngOnInit() {
+    this.product$.subscribe((data) => {
+      this.rowData = data; // Обновляем данные в гриде
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  apiKey: 'AIzaSyDmMfaoA62mWTPv2lYzJK-oDzv3NEcjW3M',
+  authDomain: 'angular-datatable-61408.firebaseapp.com',
+  projectId: 'angular-datatable-61408',
+  storageBucket: 'angular-datatable-61408.appspot.com',
+  messagingSenderId: '1078310484733',
+  appId: '1:1078310484733:web:dd4d522636e68021809c94',
+  measurementId: 'G-NKKCL28B14',
+};
